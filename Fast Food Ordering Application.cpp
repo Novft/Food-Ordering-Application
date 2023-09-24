8ece8e941ad1b5cf934602a8840525994b4b091b
leewei1702 <46920908+leewei1702@users.noreply.github.com>
2023-09-24 17:19:44 +0800

    COOL LOGO !!!

--- Fast Food Ordering Application.cpp
+++ Fast Food Ordering Application.cpp
@@ -7,6 +7,9 @@
 #include <cstdlib>
 #include <cctype>
 #include <fstream>
+#define YELLOW "\x1B[33m"
+#define RESET "\x1B[0m"
+#define CYAN "\x1B[36m"
 
 using namespace std;
 
@@ -35,9 +38,23 @@ int main()
     bool stop = false, stop2 = false;
     string option;
 
-    cout << setw(45) << setfill('-') << "\n";
-    cout << "Welcome to Fast Food Ordering Application" << endl;
-    cout << setw(45) << setfill('-') << "\n";
+    cout << YELLOW "\n############################################################################################################################################################################"
+        << "\n# ___       __    _______    ___        ________   ________   _____ ______    _______           _________   ________                                                       #"
+        << "\n#|\\  \\     |\\  \\ |\\  ___ \\  |\\  \\      |\\   ____\\ |\\   __  \\ |\\   _ \\  _   \\ |\\  ___ \\         |\\___   ___\\|\\   __  \\                                                      #"
+        << "\n#\\ \\  \\    \\ \\  \\\\ \\   __/| \\ \\  \\     \\ \\  \\___| \\ \\  \\|\\  \\\\ \\  \\\\\\__\\ \\  \\\\ \\   __/|        \\|___ \\  \\_|\\ \\  \\|\\  \\                                                     #"
+        << "\n# \\ \\  \\  __\\ \\  \\\\ \\  \\_|/__\\ \\  \\     \\ \\  \\     \\ \\  \\\\\\  \\\\ \\  \\\\|__| \\  \\\\ \\  \\_|/__           \\ \\  \\  \\ \\  \\\\\\  \\                                                    #"
+        << "\n#  \\ \\  \\|\\__\\_\\  \\\\ \\  \\_|\\ \\\\ \\  \\____ \\ \\  \\____ \\ \\  \\\\\\  \\\\ \\  \\    \\ \\  \\\\ \\  \\_|\\ \\           \\ \\  \\  \\ \\  \\\\\\  \\                                                   #"
+        << "\n#   \\ \\____________\\\\ \\_______\\\\ \\_______\\\\ \\_______\\\\ \\_______\\\\ \\__\\    \\ \\__\\\\ \\_______\\           \\ \\__\\  \\ \\_______\\                                                  #"
+        << "\n#    \\|____________| \\|_______| \\|_______| \\|_______| \\|_______| \\|__|     \\|__| \\|_______|            \\|__|   \\|_______|                                                  #"
+        << "\n# ________    _______    ___        ________   ________   ________           ________  ________   ________   _________        ________  ________   ________   ________     #"
+        << "\n#|\\   ___  \\ |\\  ___ \\  |\\  \\      |\\   __  \\ |\\   __  \\ |\\   ___  \\        |\\  _____\\|\\   __  \\ |\\   ____\\ |\\___   ___\\     |\\  _____\\|\\   __  \\ |\\   __  \\ |\\   ___ \\    #"
+        << "\n#\\ \\  \\\\ \\  \\\\ \\   __/| \\ \\  \\     \\ \\  \\|\\  \\\\ \\  \\|\\  \\\\ \\  \\\\ \\  \\       \\ \\  \\__/ \\ \\  \\|\\  \\\\ \\  \\___|_\\|___ \\  \\_|     \\ \\  \\__/ \\ \\  \\|\\  \\\\ \\  \\|\\  \\\\ \\  \\_|\\ \\   #"
+        << "\n# \\ \\  \\\\ \\  \\\\ \\  \\_|/__\\ \\  \\     \\ \\  \\\\\\  \\\\ \\  \\\\\\  \\\\ \\  \\\\ \\  \\       \\ \\   __\\ \\ \\   __  \\\\ \\_____  \\    \\ \\  \\       \\ \\   __\\ \\ \\  \\\\\\  \\\\ \\  \\\\\\  \\\\ \\  \\ \\\\ \\  #"
+        << "\n#  \\ \\  \\\\ \\  \\\\ \\  \\_|\\ \\\\ \\  \\____ \\ \\  \\\\\\  \\\\ \\  \\\\\\  \\\\ \\  \\\\ \\  \\       \\ \\  \\_|  \\ \\  \\ \\  \\\\|____|\\  \\    \\ \\  \\       \\ \\  \\_|  \\ \\  \\\\\\  \\\\ \\  \\\\\\  \\\\ \\  \\_\\\\ \\ #"
+        << "\n#   \\ \\__\\\\ \\__\\\\ \\_______\\\\ \\_______\\\\ \\_______\\\\ \\_______\\\\ \\__\\\\ \\__\\       \\ \\__\\    \\ \\__\\ \\__\\ ____\\_\\  \\    \\ \\__\\       \\ \\__\\    \\ \\_______\\\\ \\_______\\\\ \\_______\\#"
+        << "\n#    \\|__| \\|__| \\|_______| \\|_______| \\|_______| \\|_______| \\|__| \\|__|        \\|__|     \\|__|\\|__||\\_________\\    \\|__|        \\|__|     \\|_______| \\|_______| \\|_______|#"
+        << "\n#                                                                                                   \\|_________|                                                           #"
+        << "\n############################################################################################################################################################################" << RESET << endl;
 
     while (stop2 == false)
     {
@@ -74,9 +91,35 @@ int main()
             {
                 stop = true;
                 stop2 = true;
-                cout << "\n" << setw(75) << setfill('-') << "\n";
-                cout << "Thank you for using our Fast Food Ordering Application. Have a nice day." << endl;
-                cout << setw(75) << setfill('-') << "\n";
+                cout << CYAN << "\n/====================================================================================\\"
+                    << "\n|| _____  _                    _                             __                     ||"
+                    << "\n|||_   _|| |__    __ _  _ __  | | __  _   _   ___   _   _   / _|  ___   _ __        ||"
+                    << "\n||  | |  | '_ \\  / _` || '_ \\ | |/ / | | | | / _ \\ | | | | | |_  / _ \\ | '__|       ||"
+                    << "\n||  | |  | | | || (_| || | | ||   <  | |_| || (_) || |_| | |  _|| (_) || |          ||"
+                    << "\n||  |_|  |_| |_| \\__,_||_| |_||_|\\_\\  \\__, | \\___/  \\__,_| |_|   \\___/ |_|          ||"
+                    << "\n||             _                      |___/            __              _            ||"
+                    << "\n|| _   _  ___ (_) _ __    __ _    ___   _   _  _ __   / _|  __ _  ___ | |_          ||"
+                    << "\n||| | | |/ __|| || '_ \\  / _` |  / _ \\ | | | || '__| | |_  / _` |/ __|| __|         ||"
+                    << "\n||| |_| |\\__ \\| || | | || (_| | | (_) || |_| || |    |  _|| (_| |\\__ \\| |_          ||"
+                    << "\n|| \\__,_||___/|_||_| |_| \\__, |  \\___/  \\__,_||_|    |_|   \\__,_||___/ \\__|         ||"
+                    << "\n||                       |___/                                                      ||"
+                    << "\n||  __                    _                   _              _                      ||"
+                    << "\n|| / _|  ___    ___    __| |   ___   _ __  __| |  ___  _ __ (_) _ __    __ _        ||"
+                    << "\n||| |_  / _ \\  / _ \\  / _` |  / _ \\ | '__|/ _` | / _ \\| '__|| || '_ \\  / _` |       ||"
+                    << "\n|||  _|| (_) || (_) || (_| | | (_) || |  | (_| ||  __/| |   | || | | || (_| |       ||"
+                    << "\n|||_|   \\___/  \\___/  \\__,_|  \\___/ |_|   \\__,_| \\___||_|   |_||_| |_| \\__, |       ||"
+                    << "\n||                      _  _              _    _                       |___/        ||"
+                    << "\n||  __ _  _ __   _ __  | |(_)  ___  __ _ | |_ (_)  ___   _ __                       ||"
+                    << "\n|| / _` || '_ \\ | '_ \\ | || | / __|/ _` || __|| | / _ \\ | '_ \\                      ||"
+                    << "\n||| (_| || |_) || |_) || || || (__| (_| || |_ | || (_) || | | |                     ||"
+                    << "\n|| \\__,_|| .__/ | .__/ |_||_| \\___|\\__,_| \\__||_| \\___/ |_| |_|                     ||"
+                    << "\n|| _   _ |_|    |_|                          _                  _                   ||"
+                    << "\n||| | | |  __ _ __   __ ___    __ _   _ __  (_)  ___  ___    __| |  __ _  _   _     ||"
+                    << "\n||| |_| | / _` |\\ \\ / // _ \\  / _` | | '_ \\ | | / __|/ _ \\  / _` | / _` || | | |    ||"
+                    << "\n|||  _  || (_| | \\ V /|  __/ | (_| | | | | || || (__|  __/ | (_| || (_| || |_| |  _ ||"
+                    << "\n|||_| |_| \\__,_|  \\_/  \\___|  \\__,_| |_| |_||_| \\___|\\___|  \\__,_| \\__,_| \\__, | (_)||"
+                    << "\n||                                                                        |___/     ||"
+                    << "\n\\====================================================================================/" << RESET << endl;
             }
             else if (option != "1" || option != "2" || option != "3" || option != "4" || option.empty()) //Check if the choice input is empty or contain other out of bound digit
             {
@@ -238,183 +281,7 @@ void registerCustomerModule()
 //Food Service and Selection Module Function
 void foodServiceModule()
 {
-    //User Input Data
-    int foodChoice, quantity;
-    char waysToDine, foodType, orderAgain;
-    //Menu Data
-    string foods[6] = { "Burger" , "Fried Chicken" , "Nasi Lemak" , "Nuggets" , "Fries" , "Coke" };
-    float foodPrice[6] = { 8.00     , 6.00            , 10.00        , 6.00      , 3.00    , 2.00 };
-    //Order Data
-    int orderCount = 0, orderedQuantity[10];
-    char orderedFoodType[10];
-    string foodsOrdered[10];
-    float foodOrderedPrice[10];
-    //Service Charge Data
-    float charges;
-
-    //customer choosing to dine in or take away
-    cout << "Do you want to dine-in or take-away?" << endl;
-    cout << "Type : [D] Dine-In ;" << endl;
-    cout << "       [T] Take-Away" << endl;
-    cin >> waysToDine;
-    //validation of input
-    bool dineValidation = false;
-    while (dineValidation == false)
-    {
-        if (waysToDine == 'D' || waysToDine == 'd')//update dine-in charges
-        {
-            charges = 0.1;
-            dineValidation = true;
-        }
-        else if (waysToDine == 'T' || waysToDine == 't')//update take-away charges
-        {
-            charges = 2;
-            dineValidation = true;
-        }
-        else//Invalid input! User try again.
-        {
-            cout << "Invalid input! Please choose again." << endl;
-            cout << "Type : [D] Dine-In ;" << endl;
-            cout << "       [T] Take-Away" << endl;
-            cin >> waysToDine;
-        }
-    }
-    //Ordering Selection Function
-    //1st : Get User Input
-    //2nd : Store Data in Arrays
-    //3rd : Display Order Details
-    //4th : Ask to order again or stop ordering
-    for (int i = 0; i < 10; i++)//Will keep looping the Ordering Function //Until "Max Order = 10" reached or user stop ordering
-    {
-        do//1.0 : Display menu and food selection
-        {
-            int choiceCount = 0;
-            //Menu Display
-            cout << "What would you like to order?" << endl;
-            cout << "Food              || Price" << endl;
-            cout << "==================||================" << endl;
-            for (int i = 0; i < 6; i++)// loop print all the menu's foods and price with format
-            {
-                cout << left << setw(18) << setfill(' ') << foods[i]
-                    << "|| RM" << fixed << setprecision(2) << right << setw(8) << setfill(' ') << foodPrice[i]
-                    << "  [" << i + 1 << "]" << endl;
-            }
-            //let user choose the order based on numbers given
-            cout << "*Press the number inside [ ] to select your order" << endl;
-            cin >> foodChoice;
-            if (foodChoice > 6 || foodChoice <= 0)//validation for available option
-            {
-                cout << "Option does not exist. Please try again." << endl;
-                choiceCount++;
-            }
-        } while (foodChoice > 6 || foodChoice <= 0);// will keep looping until user key in the available number of foodChoice 
-
-        //1.1 : Get user to want set or ala-carte
-        cout << "Do you want Set or Ala-carte?" << endl;
-        cout << "Type : [S] for Set ;" << endl;
-        cout << "       [A] for Ala-carte ;" << endl;
-        cout << "       [B] for Both " << endl;
-        cin >> foodType;
-        //validation
-        bool foodTypeValidation = false;
-        while (foodTypeValidation == false)
-        {
-            if (foodType == 'S' || foodType == 's' || foodType == 'A' || foodType == 'a' || foodType == 'B' || foodType == 'b')
-            {
-                if (foodType == 's')//change from small to large capital
-                {
-                    foodType = 'S';
-                }
-                else if (foodType == 'a')//change from small to large capital
-                {
-                    foodType = 'A';
-                }
-                else if (foodType == 'b')//change from small to large capital
-                {
-                    foodType = 'B';
-                }
-                foodTypeValidation = true; //break validation loop
-            }
-            else //call out again to ask user to input again
-            {
-                cout << "Invalid input! Please choose again." << endl;
-                cout << "Type : [S] for Set ;" << endl;
-                cout << "       [A] for Ala-carte ;" << endl;
-                cout << "       [B] for Both " << endl;
-                cin >> foodType;
-            }
-        }
-
-        //1.2 : Get user quantity of food selected 
-        cout << "How much Quantity : ";
-        cin >> quantity;
-        //validation
-        bool quantityValidation = false;
-        while (quantityValidation == false)
-        {
-            if (quantity > 10) //can only have Max 10 quantity
-            {
-                cout << "Max quantity has reached!! Please type in a new quantity : ";
-                cin >> quantity;
-            }
-            else if (quantity <= 0) //quantity must be greater than 0
-            {
-                cout << "Please type in quantity greater than 0 : ";
-                cin >> quantity;
-            }
-            else //break validation loop
-            {
-                quantityValidation = true;
-            }
-        }
-
-        //2 : Store the data user inputed into an array based on current order count
-        foodsOrdered[orderCount] = foods[foodChoice - 1];
-        orderedFoodType[orderCount] = foodType;
-        orderedQuantity[orderCount] = quantity;
-        //calculation of single order food price *calculation included "multipled by quantity"
-        if (foodType == 'S' || foodType == 's')//if set will also add on the price of fries and coke
-        {
-            foodOrderedPrice[orderCount] = (foodPrice[foodChoice - 1] + foodPrice[4] + foodPrice[5]) * quantity;
-        }
-        else if (foodType == 'B' || foodType == 'b')//if both double the price of food choice and also add on fries and coke
-        {
-            foodOrderedPrice[orderCount] = (foodPrice[foodChoice - 1] * 2 + foodPrice[4] + foodPrice[5]) * quantity;
-        }
-        else//just add on the ala-carte price
-        {
-            foodOrderedPrice[orderCount] = foodPrice[foodChoice - 1] * quantity;
-        }
-
-        //3 : Display the data user inputed
-        cout << endl;
-        cout << "Your current order:                                            " << endl;
-        cout << "Food               Type        Quantity        Price           " << endl;
-        cout << "===============================================================" << endl;
-        for (int j = 0; j < orderCount + 1; j++)//condition needs to + 1 else first order won't display 
-        {   //Array value display with the same initialization value [j] 
-            //      else if used [orderCount] value it will only print the same exact recent order 2 times
-            cout << left << setw(18) << setfill(' ') << foodsOrdered[j] <<
-                left << setw(18) << setfill(' ') << orderedFoodType[j] <<
-                left << setw(18) << setfill(' ') << orderedQuantity[j] <<
-                left << setw(18) << setfill(' ') << foodOrderedPrice[j] << endl;
-        }
-        cout << "===============================================================" << endl;
-
-        //4 : Ask user if they want to continue to order
-        cout << "Order Again?" << endl;
-        cout << "*Press Any Button for Yes; [N] for No" << endl;
-        cin >> orderAgain;
-        if (orderAgain == 'N' || orderAgain == 'n')//Will stop when user key in "N" or "n"
-        {
-            break;
-        }
-        else //A new order count is added and loopback Ordering Function
-        {
-            orderCount++;
-        }
-    }
-    paymentModule();
+    //Insert here
 }
 
 //Membership Card top up Module Function
